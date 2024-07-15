80e8d3d29634e8605c92e1be8c79cde3c0665679
Suvorova Svetlana <suvorova.brs@gmail.com>
2024-07-15 10:29:08 +0200

    add PORT, MONGO_URL

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 const app = express();
 
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(MONGO_URL).then(()=> console.log('connected'))
 
-app.get('/', (req, res) => {
-      res.send('Hello from ...')
+app.get('/', (req, res) => { 
+      res.send('Hello ...')
 })
-app.listen(3000, ()=>{
-      console.log('App is running on port 3000')
+app.listen(PORT, ()=>{
+      console.log(`App is running on ${PORT}`)
 })
